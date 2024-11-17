e96f25c68fd935755920c315bea9420db7f116b6
pincha100 <113956174+pincha100@users.noreply.github.com>
2024-11-17 20:22:47 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from aiogram.types import Message
+
+# Ваш токен бота (замените на ваш реальный токен!)
+API_TOKEN = '7291151009:AAErK9lsTQI4STWpDktntJnXHDDkHUzqChM'
+
+# Создаем экземпляры бота и диспетчера
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+# Обработчик команды /start
+@dp.message_handler(commands=['start'])
+async def start(message: Message):
+    await message.answer("Привет! Я бот, помогающий твоему здоровью.")
+
+# Обработчик для всех остальных сообщений
+@dp.message_handler()
+async def all_messages(message: Message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+# Запуск бота
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
